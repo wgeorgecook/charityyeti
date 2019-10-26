f9667c3039efe38c55c72e4d50ee1e848919dbdf
William George Cook <wgeorgecook@gmail.com>
2019-10-26 14:30:34 -0700

    Listener on Twitter streams

--- main/main.go
+++ main/main.go
@@ -3,37 +3,74 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 	"github.com/joho/godotenv"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
 )
 
-var client *twitter.Client
-var httpClient *http.Client
-
-func init() {
+func main() {
 	// Load environment variables from .env file
+	log.Print("Loading env variables")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	// Configure global Twitter client
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("CONSUMER_KEY"),
-		ClientSecret: os.Getenv("CONSUMER_SECRET"),
-		TokenURL:     "https://api.twitter.com/oauth2/token",
+	log.Print("Configuring Twitter client")
+	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
+	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	// Verify we've connected to Twitter
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+	log.Print("Connecting to Twitter")
+	_, _, err = client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		log.Fatal("Could not authenticate to Twitter")
 	}
 
-	httpClient := config.Client(oauth2.NoContext)
-	client = twitter.NewClient(httpClient)
-}
+	// Demux allows us to use the twitter library handlers
+	// and not need to type coerce or switch on incoming
+	// messages. In this case, we're just printing out the
+	// tweet object as it comes
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		fmt.Println(tweet.Text)
+	}
 
-func main() {
-	fmt.Println("The start of something beautiful")
-	fmt.Println(fmt.Sprintf("Twitter client: %+v", client))
+	fmt.Println("Starting Stream")
+
+	// These params configure what we are filtering our string for.
+	// In this case, it's the user we're monitoring
+	// realDonaldTrump is a filler just because there is MASSIVE
+	// amounts of traffic for that account
+	filterParams := &twitter.StreamFilterParams{
+		Track:         []string{"@realDonaldTrump"},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := client.Streams.Filter(filterParams)
+	if err != nil {
+		log.Fatalf("Can't connect to stream: %v", err)
+	}
+
+	// Run the stream hander in its own goroutine
+	go demux.HandleChan(stream.Messages)
+
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	// Stop the stream
+	fmt.Println("Stopping Stream")
+	stream.Stop()
 }
