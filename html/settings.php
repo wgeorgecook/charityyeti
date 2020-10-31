622e997abb9a552a28e0511ec38c031343ccec38
jrpurdon <66390497+jrpurdon@users.noreply.github.com>
2020-10-31 10:52:15 -0400

    Add files via upload

---
+++ html/settings.php
@@ -0,0 +1,9 @@
+<?php
+define("BRAINTREE_ENV", "sandbox");
+define("BRAINTREE_MID", "wmv3nh8skrjwtqr5");
+define('BRAINTREE_PUB', "5bb5f6dxjqg8hjc7");
+define("BRAINTREE_PVT", "f65496e63149a5e558859117e0f7a86a");
+define("BRAINTREE_ATH", "sandbox_ykcw354q_wmv3nh8skrjwtqr5");
+
+define("DB_BASE", "https://charityyeti.herokuapp.com");
+?>
